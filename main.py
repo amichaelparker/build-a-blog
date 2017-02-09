cfc10f5df6e4238b800c8cad7393cca6206fe0f1
Adam Parker <amichaelparker@gmail.com>
2017-02-09 15:19:20 -0600

    basic blog functionality

--- main.py
+++ main.py
@@ -15,11 +15,58 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    post = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def get(self, id):
+        content = BlogPost.get_by_id(id)
+        self.response.write(content)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect('/blog')
+
+class RecentPosts(webapp2.RequestHandler):
+    def get(self):
+        posts = db.GqlQuery('SELECT * FROM BlogPost ORDER BY created DESC LIMIT 5')
+
+        t = jinja_env.get_template('blog.html')
+        content = t.render(posts = posts)
+        self.response.write(content)
+
+class NewPost(webapp2.RequestHandler):
+    def get(self, title="", post="", error=""):
+        t = jinja_env.get_template('newpost.html')
+        content = t.render(title = title, blogpost = post, error = error)
+        self.response.write(content)
+
+    def post(self):
+        title = self.request.get('title')
+        post = self.request.get('blogpost')
+
+        if title and post:
+            p = BlogPost(title = title, post = post)
+            p.put()
+            self.redirect('/blog')
+        else:
+            error = "Please fill out both Title and Post fields."
+            self.get(title, post, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/blog', RecentPosts),
+    ('/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
